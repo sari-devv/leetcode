78b78a9fd0579ae46ee6d591091c01b759a10074
whysoserious <abed.sari4@gmail.com>
2025-12-26 21:42:23 +0200

    LC 109: Convert Sorted List to Binary Search Tree
    Solution with extra memory, moves all the list into an array
    at the beginning.

---
+++ lc109.cpp
@@ -0,0 +1,57 @@
+#include "defaults.hpp"
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {
+    }
+    ListNode(int x) : val(x), next(nullptr) {
+    }
+    ListNode(int x, ListNode* next) : val(x), next(next) {
+    }
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {
+    }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+    }
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x),
+          left(left),
+          right(right) {
+    }
+};
+
+class Solution {
+    TreeNode* valsToBST(const vector<int>& vals, int start, int end) {
+        if (start > end) {
+            return nullptr;
+        }
+
+        int mid = (start + end) / 2;
+        assert(mid >= 0 && mid < vals.size());
+
+        TreeNode* pRoot = new TreeNode(vals[mid]);
+
+        pRoot->left = valsToBST(vals, start, mid - 1);
+        pRoot->right = valsToBST(vals, mid + 1, end);
+
+        return pRoot;
+    }
+
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        vector<int> vals;
+        ListNode* it = head;
+        while (it != nullptr) {
+            vals.push_back(it->val);
+            it = it->next;
+        }
+
+        return valsToBST(vals, 0, vals.size() - 1);
+    }
+};
